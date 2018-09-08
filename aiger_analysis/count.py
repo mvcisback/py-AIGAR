8ac800d402e23396ef98aaaca2d44872c3be53dc
Marcell Vazquez-Chanlatte <mvc@linux.com>
2018-09-08 03:03:15 -0700

    preliminary code from going from a bdd to an aiger circuit

--- aiger_analysis/count.py
+++ aiger_analysis/count.py
@@ -1,5 +1,6 @@
 import aiger
 import funcy as fn
+from parsimonious import Grammar, NodeVisitor
 
 import aiger_analysis.common as cmn
 
@@ -21,7 +22,7 @@ def to_bdd(circ, output):
         output = node_map[output]  # By name instead.
 
     bdd = BDD()
-    input_refs_to_var = {ref: f'x{i}' for i, ref in enumerate(circ.inputs)}
+    input_refs_to_var = {ref: ref for i, ref in enumerate(circ.inputs)}
     bdd.declare(*input_refs_to_var.values())
 
     gate_nodes = {}
@@ -36,6 +37,39 @@ def to_bdd(circ, output):
             gate_nodes[gate] = gate_nodes[gate.left] & gate_nodes[gate.right]
 
     return gate_nodes[output], bdd
+
+
+BDDEXPR_GRAMMAR = Grammar(u'''
+bdd_expr = ite / id / const
+ite = "ite(" id ", " bdd_expr ", " bdd_expr ")"
+id = ~r"[a-z\d]+"
+const = "TRUE" / "FALSE"
+''')
+
+
+class BDDExprVisitor(NodeVisitor):
+    def generic_visit(self, _, children):
+        return children
+
+    def visit_id(self, node, _):
+        return aiger.atom(node.text)
+
+    def visit_ite(self, _, children):
+        return aiger.ite(children[1], children[3], children[5])
+
+    def visit_bdd_expr(self, _, children):
+        return children[0]
+
+    def visit_const(self, node, _):
+        return aiger.atom(node.text == "TRUE")
+
+
+def _parse_bddexpr(ite_str: str):
+    return BDDExprVisitor().visit(BDDEXPR_GRAMMAR.parse(ite_str))
+
+
+def from_bdd(bdd_func):
+    return _parse_bddexpr(bdd_func.to_expr())
 
 
 def count(circ, percent=False, output=None):
